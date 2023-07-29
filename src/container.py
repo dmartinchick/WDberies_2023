c7fdbfa7e6373ef6b76e31ffaf36b0756336f92e
dmartinchick <dmartinchick@gmail.com>
2023-07-29 15:31:07 +0300

    add db dependency to container class

--- src/container.py
+++ src/container.py
@@ -1,6 +1,7 @@
 from dependency_injector import containers, providers
 
 from src.config import logger
+from .database import Database
 
 
 class Container(containers.DeclarativeContainer):
@@ -10,4 +11,4 @@ class Container(containers.DeclarativeContainer):
 
     config = providers.Configuration(yaml_files=['config.yml'])
 
-
+    db = providers.Singleton(Database, db_url=config.db.url)
