91966c629574744e764206fa472d42ef4e42b756
dmartinchick <dmartinchick@gmail.com>
2023-07-29 12:21:36 +0300

    created database and item schemas

---
+++ src/database.py
@@ -0,0 +1,31 @@
+from contextlib import contextmanager, AbstractContextManager
+from typing import Any
+from config import logger
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session, scoped_session, sessionmaker
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Database:
+    def __init__(self, db_url: str) -> None:
+        self._engine = create_engine(url=db_url, echo=True)
+        self._session_factory = scoped_session(sessionmaker(bind=self._engine))
+
+    def create_database(self) -> None:
+        Base.metadata.create_all(self._engine)
+
+    @contextmanager
+    def session(self) -> Any | AbstractContextManager[Session]:
+        session: Session = self._session_factory()
+        try:
+            yield session
+        except Exception:
+            logger.exception("Session rolback because of exeption")
+            session.rollback()
+            raise
+        finally:
+            session.close()
--- src/posts/item/schemas.py
+++ src/posts/item/schemas.py
@@ -1,4 +1,17 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
-class Item:
+from src.database import Base
+
+
+class Item(Base):
 
     __tablename__ = "items"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    brand: Mapped[str] = mapped_column(nullable=True)
+    img_url: Mapped[str] = mapped_column(nullable=True)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    name: Mapped[str] = mapped_column(nullable=True)
+    point: Mapped[float] = mapped_column(default=300.0)
+    price: Mapped[float] = mapped_column(nullable=True)
