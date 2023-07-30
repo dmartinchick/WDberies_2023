fafc27308b0498b7c4e4374e4cb785d324643c2a
dmartinchick <dmartinchick@gmail.com>
2023-07-30 10:51:11 +0300

    added item_repository and item_services to container

--- src/container.py
+++ src/container.py
@@ -3,6 +3,9 @@ from dependency_injector import containers, providers
 from src.config import logger
 from .database import Database
 
+from src.posts.item.repositories import ItemRepository
+from src.posts.item.services import ItemService
+
 
 class Container(containers.DeclarativeContainer):
     logger.info("Container was created!")
@@ -12,3 +15,13 @@ class Container(containers.DeclarativeContainer):
     config = providers.Configuration(yaml_files=['config.yml'])
 
     db = providers.Singleton(Database, db_url=config.db.url)
+
+    item_repository = providers.Factory(
+        ItemRepository,
+        session_factory=db.provided.session,
+    )
+
+    item_service = providers.Factory(
+        ItemService,
+        item_repository=item_repository,
+    )
